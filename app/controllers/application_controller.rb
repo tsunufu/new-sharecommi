6b261d56674a093d8454c13e71c22e4d305a4887
tsunufu <tsunufu@icloud.com>
2022-06-05 09:36:23 +0000

    ログイン画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    helper_method :current_user
+
+    def login?
+        if current_user.nil?
+            redirect_to login_path, alert: "you have to login"
+        end
+    end
+
+    def already_login?
+        unless current_user.nil?
+            redirect_to user_path, notice: "you are already login"
+        end
+    end
+
+    def current_user
+        if session[:user_id]
+            current_user ||= User.find(session[:user_id])
+        end
+    end
 end
